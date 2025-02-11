12dc21f55a11d764c90ba1b7b4ed83c90870dabe
KangSDhi <kangteknisi@gmail.com>
2025-02-11 10:05:32 +0700

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'About'
+})
+</script>
+
+<template>
+    <div class="flex items-center justify-center h-screen bg-gray-100">
+        <div class="relative max-w-5xl p-20 bg-green-300 rounded-lg shadow-lg">
+            <h2 class="text-4xl font-bold font-mono text-gray-800 underline mb-4">Tentangku</h2>
+            <p class="text-white text-3xl font-semibold font-mono indent-20">
+              Aku terbuat dari adonan tepung dan kentang, digoreng garing dalam wajan kehidupan, dihidangkan dengan saus harapan, lalu disantap oleh waktu yang selalu lapar. 🥔✨
+            </p>
+            <div class="absolute -bottom-20 -left-20">
+                <img src="../assets/Asset 5.svg" class="w-40 h-40" alt="" srcset="">
+            </div>
+            <div class="absolute -top-20 -right-15">
+              <img src="../assets/Asset 5.svg" class="w-40 h-40" alt="" srcset="">
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '../components/HelloWorld.vue';
+import About from '../components/About.vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+    About
+  }
+})
+</script>
+
+<template>
+    <HelloWorld/>
+    <About/>
+</template>
